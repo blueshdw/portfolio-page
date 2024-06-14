6dbd57322976038868083f57b2b8b8edf15aa0d7
İzzettin <134144228+blueshdw@users.noreply.github.com>
2024-06-14 12:18:07 +0300

    Update TeamData.js

--- src/TeamData.js
+++ src/TeamData.js
@@ -4,7 +4,7 @@
         name: "Sertan M. “TBase” Okcu",
         job: "Game Dev & Designer",
         explanation: "I love games. I am a game developer & designer. I was made games with Godot but now I am using Unity and C#. I meet Zeki M. in a  random comment on Instagram. He was said me “I am doing some cool 3D stuff” and now we are 2M. I try to add a pinch of my soul to the games I make. I hope you will like it",
-        profile: "https://drive.google.com/file/d/1dOOdU2n48AEopSZNxIGYrsK6OeFuBX8z/view",
+        profile: "https://ibb.co/6XXt41w",
         contact: {
             itch: "https://sertanm.itch.io/", 
             github: "https://github.com/SertanM",
@@ -17,7 +17,7 @@
         name: "Zeki M. Pehlivan",
         job: "Game Artist & Level Designer",
         explanation: "Hey I am M. Zeki Pehlivan. I am interested with 3D Design and other requiring aesthetics things like Pixel arts, illustrations and Scenario or Character etc. I love make games with my friends, we are usually have good ideas. we have fun while making games. I already feel like you love us!",
-        profile: "https://drive.google.com/file/d/13KwSr980v-pGh4w81pr8U1ma1eGtB3v-/view",
+        profile: "https://ibb.co/X3dM3ZZ",
     }
 ]
 
