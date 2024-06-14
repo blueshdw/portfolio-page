2250bc319f31f2abf4a2e12e82c566f1065c73b1
İzzettin <134144228+blueshdw@users.noreply.github.com>
2024-06-14 16:46:15 +0300

    Update GameData.js

--- src/GameData.js
+++ src/GameData.js
@@ -2,7 +2,7 @@ export const gameData = [
     {
         "id": 1,
         "image": "https://img.itch.zone/aW1nLzE2MDcwNTE5LnBuZw==/315x250%23c/ITsKmK.png",
-        "name": "Astreoid Miner[PROTOTYPE v0.011f]",
+        "name": "Mayhem Of Iris",
         "demo": "https://sertanm.itch.io/astreoidminer",
         "github": "https://github.com/SertanM/Astreoid-Miner"
     },
